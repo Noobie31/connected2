81904b33299481f0d8c074cdeaee22eb66fbc827
Noobie31 <yuvan.msky@gmail.com>
2025-10-07 06:06:01 +0530

    frlk

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,15 @@ import withPWA from "next-pwa";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true, // optional
 };
 
 export default withPWA({
   ...nextConfig,
-  dest: "public",
+  dest: "public",           // service worker output
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  // Optional: exclude some files from caching
+  // exclude: [/middleware-manifest\.json$/],
 });
